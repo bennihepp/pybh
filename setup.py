a76aaa1460db485b06758c0bf81f2ee3520f26f6
Benjamin Hepp <benjamin.hepp@posteo.de>
2018-06-08 23:16:06 +0200

    updated transformations contrib

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import numpy
+from distutils.core import setup, Extension
+
+transformations_module = Extension(
+    'pybh.contrib._transformations',
+    #define_macros = [('MAJOR_VERSION', '1'),
+    #                 ('MINOR_VERSION', '0')],
+    include_dirs = [numpy.get_include()],
+    libraries = [],
+    library_dirs = [],
+    sources=['pybh/contrib/transformations.c'])
 
 setup(name='pybh',
       version='0.2',
@@ -9,6 +19,7 @@ setup(name='pybh',
       author_email='benjamin.hepp@posteo.de',
       license='BSD 3 License',
       packages=['pybh'],
+      ext_modules=[transformations_module],
       install_requires=[
           'numpy',
           'Pillow',
